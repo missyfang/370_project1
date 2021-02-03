5b688bf4bbd3ee00817310be40271742474e53f6
msfang <msfang@umich.edu>
2021-02-03 09:20:46 -0700

    Initial Commit

---
+++ 370_project1/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  370_project1
+//
+//  Created by Missy Fang  on 2/3/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
